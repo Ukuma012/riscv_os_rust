d5590aad9f288061e61feb80a50484535780dcf1
Ukuma012 <ukuma012@gmail.com>
2025-03-02 14:25:39 +0900

    open sbi

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+#![feature(naked_functions)]
+
+use core::{panic::PanicInfo, ptr};
+use core::arch::naked_asm;
+
+unsafe extern "C" {
+    static mut __bss: u32;
+    static __bss_end: u32;
+    static __stack_top: u32;
+}
+
+#[unsafe(no_mangle)]
+fn kernel_main() {
+    unsafe {
+        let bss = ptr::addr_of_mut!(__bss);
+        let bss_end = ptr::addr_of!(__bss_end);
+        ptr::write_bytes(bss, 0, bss_end as usize - bss as usize);
+    }
+
+    loop {}
+}
+
+#[unsafe(link_section = ".text.boot")]
+#[unsafe(no_mangle)]
+#[naked]
+extern "C" fn boot() {
+    unsafe {
+        naked_asm!(
+            "la sp, {stack_top}",
+            "j kernel_main",
+            stack_top = sym  __stack_top,
+        );
+    }
+}
+
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
+    loop {}
 }
