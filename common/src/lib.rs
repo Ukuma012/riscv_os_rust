fca6b1054ffef96055320f37f4d35b5aef9b5c4b
Ukuma012 <ukuma012@gmail.com>
2025-03-03 22:21:09 +0900

    read csr write csr

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -43,3 +43,29 @@ macro_rules! println {
         $crate::print!("{}\n", format_args!($($arg)*));
     }
 }
+
+#[macro_export]
+macro_rules! read_csr {
+    ($csr:expr) => {
+        unsafe {
+            use core::arch::asm;
+            let mut csrr: u32;
+            asm!(
+                concat!("csrr {r}, ", $csr), r = out(reg) csrr
+            );
+            csrr
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! write_csr {
+    ($csr:expr, $value:expr) => {
+        unsafe {
+            use core::arch:asm;
+            asm!(
+                concat!("csrw ", $csr, ", {r}"), r = in(reg) $value
+            );
+        }
+    };
+}
